644e606c01aa0bc88ca47bca47d44102ceda143b
Piyush Singh <mrsinghpiyush@gmail.com>
2017-03-07 16:04:08 +0000

    RWA wip

--- tests/python/unittest/test_rnn.py
+++ tests/python/unittest/test_rnn.py
@@ -1,6 +1,14 @@
 import find_mxnet
 import mxnet as mx
 import numpy as np
+import pdb as pdb
+pds = pdb.set_trace
+
+import sys
+sys.path.append('/v/home/epiyusi/private/Downloads/github/mxnetUtils')
+from mxnetUtils import printStats, _str2tuple, net2dot
+
+shape = {}
 
 def test_rnn():
     cell = mx.rnn.RNNCell(100, prefix='rnn_')
@@ -23,6 +31,33 @@ def test_lstm():
     assert outs == [(10, 100), (10, 100), (10, 100)]
 
 def test_rwa():
+
+    inputs = mx.symbol.Variable('data')
+    next_c = mx.symbol._internal._minimum_scalar(inputs, scalar=1, name='state')
+
+    net = next_c
+    #net = inputs
+
+    executor = net.bind(mx.cpu(), {'data': mx.nd.array([0, 0.9, 1.1, 2, 3]) })
+
+    res = executor.forward()
+    print(res[0].asnumpy())
+
+
+
+
+    shape["data"] = (10,100)
+    printStats(net, shape=shape)
+
+    pds()
+
+
+
+
+
+    return
+
+
     cell = mx.rnn.RWACell(100, prefix='rnn_')
     outputs, _ = cell.unroll(3, input_prefix='rnn_')
     outputs = mx.sym.Group(outputs)
